34900a277e6eb780ad7a72672805b4b215551141
Suresh <surysharma@gmail.com>
2012-03-10 12:03:21 +0000

    Added the first code for ant

--- src/com/antinaction/FirstBuild.java
+++ src/com/antinaction/FirstBuild.java
@@ -1,11 +1,7 @@
 package com.antinaction;
 
-/**
- * Created by IntelliJ IDEA.
- * User: sureshsharma
- * Date: 10/03/2012
- * Time: 11:57
- * To change this template use File | Settings | File Templates.
- */
 public class FirstBuild {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
 }
