4bcb49359fa480fd89cef404435b94ca13b10853
Suresh <surysharma@gmail.com>
2012-04-22 00:00:51 +0100

    Persistant class without getters and setters

---
+++ bookstore/src/com/hedgefunds/domain/Event.java
@@ -0,0 +1,11 @@
+package com.hedgefunds.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sureshsharma
+ * Date: 21/04/2012
+ * Time: 23:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Event {
+}
