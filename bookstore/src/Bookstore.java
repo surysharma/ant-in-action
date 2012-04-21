10761d804b349fce22c2a9b43138e9a3ef2ec380
Suresh <surysharma@gmail.com>
2012-04-21 23:39:32 +0100

    Moved hibernate.cfg.xml to root of the classpath

--- bookstore/src/Bookstore.java
+++ bookstore/src/Bookstore.java
@@ -1,11 +1,15 @@
 import com.hedgefunds.services.SessionFactoryUtil;
 import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
 
 public class Bookstore {
     private static SessionFactoryUtil sessionFactoryUtil;
 
     public static void main(String[] args) {
         SessionFactory sf = SessionFactoryUtil.sf;
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.getTransaction().commit();
         System.out.println("Bookstore...");
     }
 }
--- bookstore/src/com/hedgefunds/services/SessionFactoryUtil.java
+++ bookstore/src/com/hedgefunds/services/SessionFactoryUtil.java
@@ -4,14 +4,12 @@ package com.hedgefunds.services;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.io.File;
-
 public class SessionFactoryUtil {
     public static SessionFactory sf = createSessionFactory();
 
     private static SessionFactory createSessionFactory() {
         Configuration configuration = new Configuration();
-        configuration.configure(new File("bookstore/hibernate.cfg.xml"));
+        configuration.configure();
         return configuration.buildSessionFactory();
     }
 }
